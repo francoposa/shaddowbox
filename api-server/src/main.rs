26d180377e85a1a4c0017a761dfc40e718127261
francoposa <franco@francoposa.io>
2023-01-04 19:36:01 -0800

    api application structure with simplified bytes echo

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,16 +1,12 @@
-use std::collections::HashMap;
+mod application;
 
+use application::server::file_handler;
 use axum::{
-    body::Body,
-    http::Request,
     routing::{get, post},
-    Json, Router,
+    Router,
 };
 use hyper::server::Server;
-use serde::Serialize;
-use serde_json::{json, Value};
 use tracing::level_filters::LevelFilter;
-use tracing::instrument;
 use tracing_bunyan_formatter::BunyanFormattingLayer;
 use tracing_subscriber::{prelude::*, Registry};
 
@@ -23,52 +19,13 @@ async fn main() {
         BunyanFormattingLayer::new(SERVICE_NAME.into(), std::io::stderr)
             .with_filter(LevelFilter::INFO);
 
-    let subscriber = Registry::default()
-        .with(std_stream_bunyan_format_layer);
+    let subscriber = Registry::default().with(std_stream_bunyan_format_layer);
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
-    let app = Router::new()
-        .route("/", get(echo))
-        .route("/", post(echo));
+    let app = Router::new().route("/", post(file_handler::put));
 
     Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
-}
-
-#[derive(Serialize, Debug)]
-struct EchoResponse {
-    method: String,
-    headers: HashMap<String, String>,
-    body: String,
-}
-
-#[instrument]
-async fn echo(request: Request<Body>) -> Json<Value> {
-    let (req_parts, req_body) = request.into_parts();
-
-    let req_method = req_parts.method.to_string();
-
-    let parsed_req_headers = req_parts
-        .headers
-        .iter()
-        .map(|(k, v)| (k.to_string(), v.to_str().unwrap_or_default().to_string()))
-        .collect::<HashMap<String, String>>();
-
-    let parsed_req_body = match hyper::body::to_bytes(req_body).await {
-        Ok(bytes) => match String::from_utf8(bytes.to_vec()) {
-            Ok(str) => str,
-            Err(_) => String::new(),
-        },
-        Err(_) => String::new(),
-    };
-
-    let resp_body = EchoResponse {
-        method: req_method,
-        headers: parsed_req_headers,
-        body: parsed_req_body,
-    };
-
-    Json(json!(resp_body))
 }
