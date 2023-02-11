5d27ed3ebc73579432beee54b4d9f0042ed8da38
francoposa <franco@francoposa.io>
2023-02-11 10:14:21 -0800

    extract content-length parsing with expanded error handling

--- core/src/application/server/object_handler.rs
+++ core/src/application/server/object_handler.rs
@@ -2,9 +2,9 @@ use crate::application::server::util;
 use crate::domain::object::ObjectStream;
 use crate::domain::object_service::ObjectService;
 use axum::extract::{BodyStream, Path, State};
-use axum::http;
 use axum::http::{HeaderMap, Method, StatusCode};
 use futures_util::TryStreamExt;
+use std::fmt::format;
 use std::sync::Arc;
 use tracing::instrument;
 
@@ -25,15 +25,7 @@ pub async fn put_object(
     let body_stream_with_io_error = body_stream.map_err(|err| util::map_axum_to_std_io_err(err));
     let mut stream_reader = util::to_tokio_async_read(body_stream_with_io_error.into_async_read());
 
-    let content_length = match headers.get("content-length") {
-        Some(header_val) => header_val.to_str().unwrap().parse::<usize>().unwrap(),
-        None => {
-            return Err((
-                http::StatusCode::BAD_REQUEST,
-                String::from("missing Content-Length header"),
-            ))
-        }
-    };
+    let content_length = util::get_content_length(headers)?;
 
     let mut object = ObjectStream::new(key, Box::new(stream_reader), content_length);
     handler.object_service.put_object(object).await;
--- core/src/application/server/util.rs
+++ core/src/application/server/util.rs
@@ -1,7 +1,38 @@
+use axum::http::{HeaderMap, StatusCode};
+
 pub fn to_tokio_async_read(r: impl futures::io::AsyncRead) -> impl tokio::io::AsyncRead {
     tokio_util::compat::FuturesAsyncReadCompatExt::compat(r)
 }
 
 pub fn map_axum_to_std_io_err(err: axum::Error) -> std::io::Error {
     std::io::Error::new(std::io::ErrorKind::Other, err)
+}
+
+pub fn get_content_length(headers: HeaderMap) -> Result<usize, (StatusCode, String)> {
+    let content_length = match headers.get("content-length") {
+        Some(header_val) => match header_val.to_str() {
+            Ok(header_val) => match header_val.parse::<usize>() {
+                Ok(content_length) => content_length,
+                Err(e) => {
+                    return Err((
+                        StatusCode::BAD_REQUEST,
+                        format!("error parsing header content-length: {}", e.to_string()),
+                    ))
+                }
+            },
+            Err(e) => {
+                return Err((
+                    StatusCode::BAD_REQUEST,
+                    format!("error parsing header content-length: {}", e.to_string()),
+                ))
+            }
+        },
+        None => {
+            return Err((
+                StatusCode::BAD_REQUEST,
+                String::from("missing content-length header"),
+            ))
+        }
+    };
+    Ok(content_length)
 }
