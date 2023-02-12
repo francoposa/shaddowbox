4ce30319238db3d910613de042bf0b6c789b6097
francoposa <franco@francoposa.io>
2023-02-12 10:45:29 -0800

    slightly more elegant error handling for parsing content-length

--- core/src/application/server/util.rs
+++ core/src/application/server/util.rs
@@ -10,29 +10,26 @@ pub fn get_content_length(
     headers: axum::http::HeaderMap,
 ) -> Result<usize, (axum::http::StatusCode, String)> {
     let content_length = match headers.get("content-length") {
-        Some(header_val) => match header_val.to_str() {
-            Ok(header_val) => match header_val.parse::<usize>() {
-                Ok(content_length) => content_length,
-                Err(e) => {
-                    return Err((
-                        StatusCode::BAD_REQUEST,
-                        format!("error parsing header content-length: {}", e.to_string()),
-                    ))
-                }
-            },
+        Some(header_val) => match header_val
+            .to_str()
+            .map_err(|e| e.to_string())
+            .and_then(|header_val| header_val.parse::<usize>().map_err(|e| e.to_string()))
+        {
+            Ok(content_length) => content_length,
             Err(e) => {
                 return Err((
-                    StatusCode::BAD_REQUEST,
-                    format!("error parsing header content-length: {}", e.to_string()),
+                    axum::http::StatusCode::BAD_REQUEST,
+                    format!("error parsing header content-length: {}", e),
                 ))
             }
         },
         None => {
             return Err((
-                StatusCode::BAD_REQUEST,
+                axum::http::StatusCode::BAD_REQUEST,
                 String::from("missing content-length header"),
             ))
         }
     };
+
     Ok(content_length)
 }
